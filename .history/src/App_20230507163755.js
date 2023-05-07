0862daf5eb3eb3740fa9402421b21e2868437d8b
hoahoang25092001 <hquanghoa25092001@gmail.com>
2023-05-07 16:47:15 +0700

    v2

---
+++ .history/src/App_20230507163755.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+    <div className="flex h-screen items-center border border-red-500 justify-center">
+      <input
+      type="text"
+      className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+      />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20230507164051.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+    <div className="flex h-screen items-center border border-red-500 justify-center">
+      <input
+      type="text"
+      className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+      />
+      <button
+      type="button"
+      className="outline-none px-6 py-2 bg-blue-500 rounded-md text-white"
+      >
+      Add
+      </button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
